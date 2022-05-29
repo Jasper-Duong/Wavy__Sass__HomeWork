bf854630ae0846483707edd0eced879ee3db7826
Jasper-Duong <duongngochung13@gmail.com>
2022-05-29 22:19:05 +0700

    Responsive Navbar

--- js/style.js
+++ js/style.js
@@ -1,25 +1,28 @@
-function getEleId(id){
+function getEleId(id) {
   return document.getElementById(id);
 }
-function getEle(selector){
+function getEle(selector) {
   return document.querySelector(selector);
 }
-// if (window.scrollY > 20){
-//   getEleId('logo').style.display = 'none';
-// }
 
-window.onscroll = function(){
-  if(window.scrollY >250){
-    getEleId('logo').style.display = 'none';
-    document.querySelectorAll('.nav-item').forEach(function(ele){
-      ele.classList.add('scrollNavItem');
-    })
-    getEle('header').style.backgroundColor = 'white';
+window.onscroll = function () {
+  console.log(scrollY);
+  if (window.scrollY > 250) {
+    getEleId("logo").style.display = "none";
+    // getEle(".logo__mobile img").style.display = "none";
+    document.querySelectorAll(".nav-item").forEach(function (ele) {
+      ele.classList.add("scrollNavItem");
+    });
+    getEle("header").style.backgroundColor = "white";
   } else {
-    getEleId('logo').style.display = 'block';
-    document.querySelectorAll('.nav-item').forEach(function(ele){
-      ele.classList.remove('scrollNavItem');
-    })
-    getEle('header').style.backgroundColor = 'transparent';
+    getEleId("logo").style.display = "block";
+    // getEle(".logo__mobile img").style.display = "block";
+    document.querySelectorAll(".nav-item").forEach(function (ele) {
+      ele.classList.remove("scrollNavItem");
+    });
+    if (window.innerWidth > 1199) {
+      console.log("hey")
+      getEle("header").style.backgroundColor = "transparent";
+    }
   }
-}
+};
