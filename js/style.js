0648b96d83f84cd837de997368474a7afc9c87b6
Jasper-Duong <duongngochung13@gmail.com>
2022-05-29 21:30:53 +0700

    Add #intro section

---
+++ js/style.js
@@ -0,0 +1,25 @@
+function getEleId(id){
+  return document.getElementById(id);
+}
+function getEle(selector){
+  return document.querySelector(selector);
+}
+// if (window.scrollY > 20){
+//   getEleId('logo').style.display = 'none';
+// }
+
+window.onscroll = function(){
+  if(window.scrollY >250){
+    getEleId('logo').style.display = 'none';
+    document.querySelectorAll('.nav-item').forEach(function(ele){
+      ele.classList.add('scrollNavItem');
+    })
+    getEle('header').style.backgroundColor = 'white';
+  } else {
+    getEleId('logo').style.display = 'block';
+    document.querySelectorAll('.nav-item').forEach(function(ele){
+      ele.classList.remove('scrollNavItem');
+    })
+    getEle('header').style.backgroundColor = 'transparent';
+  }
+}
